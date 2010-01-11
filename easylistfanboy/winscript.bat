1dba8b441115bd7a6c626ef589cc1c34984f572f
mdew@mdew-desktop <mdew@mdew-desktop>
2010-01-11 14:14:11 +1300

    argh

--- easylistfanboy/winscript.bat
+++ easylistfanboy/winscript.bat
@@ -7,15 +7,15 @@ perl addChecksum.pl fanboy-adblocklist-current-expanded.txt
 perl addChecksum.pl fanboy-adblocklist-stats.txt  
 perl addChecksum.pl fanboy-adblocklist-dimensions-v2.txt
 :: Firefox Regional lists
-perl addChecksum.pl firefox-regional/fanboy-adblocklist-cz.txt
-perl addChecksum.pl firefox-regional/fanboy-adblocklist-chn.txt
-perl addChecksum.pl firefox-regional/fanboy-adblocklist-krn.txt
-perl addChecksum.pl firefox-regional/fanboy-adblocklist-jpn.txt
-perl addChecksum.pl firefox-regional/fanboy-adblocklist-esp.txt
-perl addChecksum.pl firefox-regional/fanboy-adblocklist-tky.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-cz.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-chn.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-krn.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-jpn.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-esp.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-tky.txt
 :: Opera
-perl addChecksum-opera.pl opera/urlfilter.ini
-perl addChecksum.pl opera/fanboy-adblocklist-elements-v3.css
+perl addChecksum-opera.pl opera\urlfilter.ini
+perl addChecksum.pl opera\fanboy-adblocklist-elements-v3.css
 :: Iron
 perl addChecksum-opera.pl iron/adblock-beta.ini
 :: Now sync
