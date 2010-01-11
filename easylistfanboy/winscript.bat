2b69e3d52d0d7dd866342b74401237d4a2c60b21
mdew@mdew-desktop <mdew@mdew-desktop>
2010-01-11 14:11:05 +1300

    Fix comments in winscript

--- easylistfanboy/winscript.bat
+++ easylistfanboy/winscript.bat
@@ -1,24 +1,24 @@
 @echo off
 hg pull
 hg update
-; Firefox
+:: Firefox
 perl addChecksum.pl fanboy-adblocklist-adult.txt
 perl addChecksum.pl fanboy-adblocklist-current-expanded.txt
 perl addChecksum.pl fanboy-adblocklist-stats.txt  
 perl addChecksum.pl fanboy-adblocklist-dimensions-v2.txt
-; Firefox Regional lists
+:: Firefox Regional lists
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-cz.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-chn.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-krn.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-jpn.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-esp.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-tky.txt
-; Opera
+:: Opera
 perl addChecksum-opera.pl opera/urlfilter.ini
 perl addChecksum.pl opera/fanboy-adblocklist-elements-v3.css
-; Iron
+:: Iron
 perl addChecksum-opera.pl iron/adblock-beta.ini
-; Now sync
+:: Now sync
 hg add .
 hg commit -m "%1"
 hg push -f
