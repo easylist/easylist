b9b5d6d1e3adc565dc8154d38b0d6cb68dc3c6c3
Some nzguy <some.nzguy@gmail.com>
2010-01-17 00:52:01 +1300

    merge me

--- easylistfanboy/winscript.bat
+++ easylistfanboy/winscript.bat
@@ -1,7 +1,7 @@
 @echo off
 hg pull
-hg update
 hg merge
+hg update
 :: Firefox
 perl addChecksum.pl fanboy-adblocklist-adult.txt
 perl addChecksum.pl fanboy-adblocklist-current-expanded.txt
@@ -15,6 +15,7 @@ perl addChecksum.pl firefox-regional\fanboy-adblocklist-jpn.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-esp.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-tky.txt
 :: Opera
+:: test
 perl addChecksum-opera.pl opera\urlfilter.ini
 perl addChecksum.pl opera\fanboy-adblocklist-elements-v3.css
 :: Iron
