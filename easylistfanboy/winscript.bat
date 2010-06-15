51a6b424900762ffa2e000a348934500f346a4d0
mp3geek@gmail.com <mp3geek@gmail.com>
2010-06-15 13:35:10 +1200

    update scripts

--- easylistfanboy/winscript.bat
+++ easylistfanboy/winscript.bat
@@ -8,6 +8,7 @@ perl addChecksum.pl fanboy-adblocklist-current-expanded.txt
 perl addChecksum.pl fanboy-adblocklist-stats.txt  
 perl addChecksum.pl fanboy-adblocklist-dimensions-v2.txt
 perl addChecksum.pl fanboy-adblocklist-current-p2p.txt
+perl addChecksum.pl fanboy-adblocklist-stats-intl.txt
 perl addChecksum.pl adblock-gannett.txt
 :: Firefox Regional lists
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-chn.txt
@@ -16,7 +17,6 @@ perl addChecksum.pl firefox-regional\fanboy-adblocklist-esp.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-ita.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-jpn.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-krn.txt
-perl addChecksum.pl firefox-regional\fanboy-adblocklist-rus.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-rus-v2.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-swe.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-tky.txt
