c7e621b4dfc925dfe5adc0bda65f8824c90ae5c3
Nitrox <nitrox202@gmail.com>
2010-07-30 11:07:11 +0100

    Tamil Regional list for firefox

--- easylistfanboy/winscript.bat
+++ easylistfanboy/winscript.bat
@@ -22,6 +22,7 @@ perl addChecksum.pl firefox-regional\fanboy-adblocklist-rus-v2.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-swe.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-tky.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-vtn.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-tam.txt
 :: Opera
 perl addChecksum.pl opera\fanboy-adblocklist-elements-v3.css
 perl addChecksum-opera.pl opera\urlfilter.ini
