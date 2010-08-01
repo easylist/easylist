8b219670e60f65f31374feb44c1a2980fb1492e6
mp3geek@gmail.com <mp3geek@gmail.com>
2010-08-01 23:54:16 +1200

    sync

--- easylistfanboy/winscript.bat
+++ easylistfanboy/winscript.bat
@@ -22,7 +22,7 @@ perl addChecksum.pl firefox-regional\fanboy-adblocklist-rus-v2.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-swe.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-tky.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-vtn.txt
-perl addChecksum.pl firefox-regional\fanboy-adblocklist-tam.txt
+perl addChecksum.pl firefox-regional\fanboy-adblocklist-ind.txt
 perl addChecksum.pl firefox-regional\fanboy-adblocklist-pol.txt
 :: Opera
 perl addChecksum.pl opera\fanboy-adblocklist-elements-v3.css
