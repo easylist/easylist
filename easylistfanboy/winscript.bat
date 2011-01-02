b149cfc086225a288f4eddbb93bdcebc1a26b5ae
mp3geek@gmail.com <mp3geek@gmail.com>
2011-01-02 17:02:35 +1300

    sync

--- easylistfanboy/winscript.bat
+++ easylistfanboy/winscript.bat
@@ -2,10 +2,6 @@
 hg update
 hg pull --rebase remote
 hg pull
-:: Kill any lines less than 3chars automatically
-perl -i.bak -n -e "{print if /.{4}/}" fanboy-adblocklist-current-expanded.txt
-perl -i.bak -n -e "{print if /.{4}/}" fanboy-adblocklist-stats.txt
-perl -i.bak -n -e "{print if /.{4}/}" fanboy-adblocklist-addon.txt
 del /f *.bak
 :: Firefox
 perl addChecksum.pl fanboy-adblocklist-adult.txt
@@ -35,6 +31,10 @@ perl addChecksum.pl opera\fanboy-adblocklist-elements-v3.css
 perl addChecksum-opera.pl opera\urlfilter.ini
 :: Iron
 perl addChecksum-opera.pl iron/adblock-beta.ini
+:: Kill any lines less than 3chars automatically
+perl -i.bak -n -e "{print if /.{4}/}" fanboy-adblocklist-current-expanded.txt
+perl -i.bak -n -e "{print if /.{4}/}" fanboy-adblocklist-stats.txt
+perl -i.bak -n -e "{print if /.{4}/}" fanboy-adblocklist-addon.txt
 :: Now sync
 hg add .
 hg commit -m "%*"
