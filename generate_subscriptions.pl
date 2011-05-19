94fa771e75a0e70400a4e08b614703cc65174b30
Michael <none@none>
2011-05-19 07:42:10 +0100

    M: Removed generate_subscriptions.pl and the associated licence file from the repository

--- generate_subscriptions.pl
+++
@@ -1,7 +0,0 @@
-# EasyList subscription generation update notice
-#
-# This file has been superceded by "combineSubscriptions.py", which is
-# located in the EasyList repository. Please update your systems
-# accordingly, as "generate_subscriptions.pl" will eventually be
-# removed from the repository.
-#
