7d3466b8cee28c695c9574b2b44f9f88f067c6c2
Michael <none@none>
2011-09-14 15:31:10 +0100

    M: Significantly improved FOP documentation in version 2.994

--- FOP.py
+++ FOP.py
@@ -16,11 +16,11 @@
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>."""
 # FOP version number
-VERSION = 2.993
+VERSION = 2.994
 
 # Import the key modules
 import os, re, subprocess, sys
-# The following module is only available in Python 3; if it is not available, exit
+# Attempt to import a module only available in Python 3 and raise an exception if it is not present
 try:
     from urllib.parse import urlparse
 except ImportError:
@@ -30,46 +30,48 @@ except ImportError:
 parts = re.match
 setpattern = re.compile
 
-# The following patterns are either taken from or based on Wladimir Palant's Adblock Plus source code
+# Compile some regular expressions to match important filter parts (taken from Wladimir Palant's Adblock Plus source code)
 DOMAINPATTERN = setpattern(r"^([^\/\*\|\@\"\!]*?)##")
 ELEMENTPATTERN = setpattern(r"^([^\/\*\|\@\"\!]*?)##([^{}]+)$")
 OPTIONPATTERN = setpattern(r"^(.*)\$(~?[\w\-]+(?:=[^,\s]+)?(?:,~?[\w\-]+(?:=[^,\s]+)?)*)$")
 
-# The following patterns match element tags and pseudo classes; "@" indicates either the beginning or the end of a selector
+# Compile regular expressions that match element tags and pseudo classes; "@" indicates either the beginning or the end of a selector
 SELECTORPATTERN = setpattern(r"(?<=[\s\[@])([a-zA-Z]*[A-Z][a-zA-Z]*)((?=([\[\]\^\*\$=:@]))|(?=(\s[+>])))")
 PSEUDOPATTERN = setpattern(r"(\:[a-zA-Z\-]*[A-Z][a-zA-Z\-]*)(?=([\(\:\@\s]))")
 REMOVALPATTERN = setpattern(r"((?<=(@))|(?<=([>+]\s)))([a-zA-Z]+)(?=([#\.]))")
 
-# The following pattern describes a completely blank line
+# Compile a regular expression that describes a completely blank line
 BLANKPATTERN = setpattern(r"^\s*$")
 
-# The following pattern identifies the sections of commit messages
+# Compile a regular expression that validates commit comments
 COMMITPATTERN = setpattern(r"^(A|M|P)\:\s(\((.+)\)\s)?(.*)$")
 
-# Files with the following names should not be sorted, either because they have a special sorting system or because they are not filter files
+# List the files that should not be sorted, either because they have a special sorting system or because they are not filter files
 IGNORE = ("CC-BY-SA.txt", "easytest.txt", "GPL.txt", "MPL.txt")
 
-# The following is a tuple of the Adblock Plus options, as of version 1.3.9. The domain option has been intentionally omited because it is handled separately.
+# List all Adblock Plus options (excepting domain, which is handled separately), as of version 1.3.9.
 KNOWNOPTIONS =  ("collapse", "document", "donottrack", "elemhide",
                 "image", "object", "object-subrequest", "other",
                 "match-case", "script", "stylesheet", "subdocument",
                 "third-party", "xbl", "xmlhttprequest")
 
-# The following are commands for various version control systems; only the HG commands have been throughly tested
+# List the commands used by different version control systems
 GIT = (("git", "diff"), ("git", "commit", "-m"), ("git", "pull"), ("git", "push"))
 HG = (("hg", "diff"), ("hg", "commit", "-m"), ("hg", "pull"), ("hg", "push"))
 SVN = (("svn", "diff"), ("svn", "commit", "-m"), ("svn", "update"))
 REPOTYPES = (("./.git/", GIT), ("./.hg/", HG), ("./.svn/", SVN))
 
 def start ():
-    # Print the name and version of the program
+    """ Print a greeting message and run FOP in the directories
+    specified via the command line, or the current working directory if
+    no arguments have been passed."""
     greeting = "FOP (Filter Orderer and Preener) version {version}".format(version = VERSION)
     characters = len(str(greeting))
     print("=" * characters)
     print(greeting)
     print("=" * characters)
 
-    # Run the program in each of the locations specified in the command line, or the current working directory if no location is specified
+    # Convert the directory names to absolute references and visit each unique location
     places = sys.argv[1:]
     if places:
         absplaces = [os.path.normpath(os.path.abspath(place)) for place in places]
@@ -80,7 +82,9 @@ def start ():
         main((os.path.normpath(os.path.abspath(os.getcwd()))))
 
 def main (location):
-    # Change to the specified directory if it exists
+    """ Find and sort all the files in a given directory, committing
+    changes to a repository if one exists"""
+    # Change the working directory to the specified directory if it exists, otherwise return
     if os.path.isdir(location):
         os.chdir(location)
     else:
@@ -93,14 +97,14 @@ def main (location):
         if os.path.isdir(os.path.normpath(repotype[0])):
             repository = repotype[1]
             break
-    # Record the initial changes; if this fails, give up trying to use the repository
+    # If this is a repository, record the initial changes to; if this fails, give up trying to use the repository
     if repository:
         try:
             originaldifference = True if subprocess.check_output(repository[0]) else False
         except(subprocess.CalledProcessError, OSError):
             print("The command \"{command}\" was unable to run; FOP will therefore not attempt to use the repository tools. On Windows, this may be an indication that you do not have sufficient privileges to run FOP - the exact reason why is unknown. Please also ensure that your revision control system is installed correctly and understood by FOP.".format(command = repository[0]))
             repository == None
-
+    # Work through the directory and any subdirectories, ignoring hidden directories
     print("\nPrimary location: {folder}{separator}".format(folder = location, separator = os.sep))
     for path, directories, files in os.walk(location):
         print("Current directory: {folder}{separator}".format(folder = os.path.abspath(path), separator = os.sep))
@@ -114,12 +118,12 @@ def main (location):
             # Sort all text files that are not blacklisted
             if extension == ".txt" and filename not in IGNORE:
                 fopsort(address)
-            # Delete unnecessary backups and temporary files if they are found
+            # Delete unnecessary backups and temporary files
             if extension == ".orig" or extension == ".temp":
                 try:
                     os.remove(address)
                 except(IOError, OSError):
-                    # The file has likely already been deleted
+                    # Ignore errors resulting from deleting an unnecessary file, as they likely indicate that the file has already been deleted
                     pass
 
     # Offer to commit any changes if in a repository
@@ -127,6 +131,8 @@ def main (location):
         commit(repository, location, originaldifference)
 
 def fopsort (filename):
+    """ Sort the sections of the file and save the changes, if any have
+    been made."""
     changed = False
     temporaryfile = "{filename}.temp".format(filename = filename)
     CHECKLINES = 10
@@ -139,11 +145,11 @@ def fopsort (filename):
     with open(filename, "r", encoding="utf-8", newline="\n") as inputfile, open(temporaryfile, "w", encoding="utf-8", newline="\n") as outputfile:
         for originalline in inputfile:
             line = originalline.strip()
-            # Blank lines are removed
+            # Remove blank lines and mark that the file has changed
             if parts(BLANKPATTERN, line):
                 changed = True
             else:
-                # Ignore comments and, if applicable, sort the preceding section of filters and add them to the new version of the file
+                # Ignore comments and, if applicable, sort the preceding section of filters and save them in the new version of the file
                 if line[0] == "!" or line[:8] == "%include" or line[0] == "[" and line[-1] == "]":
                     if section:
                         if elementlines > filterlines:
@@ -155,7 +161,7 @@ def fopsort (filename):
                         filterlines = elementlines = 0
                     outputfile.write("{line}\n".format(line = line))
                 else:
-                    # Neaten up filters, checking their type if necessary
+                    # Neaten up filters and, if necessary, check their type for the sorting algorithm
                     elementparts = parts(ELEMENTPATTERN, line)
                     if elementparts:
                         domains = elementparts.group(1).lower()
@@ -169,21 +175,21 @@ def fopsort (filename):
                         if newsectionline <= CHECKLINES:
                             filterlines += 1
                         line = filtertidy(line)
-                    # Add the filter to the present section
+                    # Add the filter to the present section, marking if it is different
                     if "{line}\n".format(line = line) != originalline:
                         changed = True
                     section.append(line)
                     newsectionline += 1
-        # At the end of the file, sort and add any remaining filters and add a newline regardless
+        # At the end of the file, sort and save any remaining filters
         if section:
             if elementlines > filterlines:
                 outputfile.write("{filters}\n".format(filters = "\n".join(sorted(set(section), key=lambda rule: substitute(DOMAINPATTERN, "", rule)))))
             else:
                 outputfile.write("{filters}\n".format(filters = "\n".join(sorted(set(section)))))
 
-    # Only save if changes have been made to the file, including newline corrections
+    # Only replace the existing file with the new one if it is different
     if changed:
-        # Windows requires a file to be renamed to a name not already in use
+        # Check the operating system and, if it is Windows, delete the old file to avoid an exception (you are unable to rename files to names already in use)
         if os.name == "nt":
             os.remove(filename)
         os.rename(temporaryfile, filename)
@@ -192,10 +198,11 @@ def fopsort (filename):
         os.remove(temporaryfile)
 
 def filtertidy (filterin):
-    # If applicable, sort options
+    """ Sort the options of blocking filters and make the filter text
+    lower case if applicable."""
     optionsplit = parts(OPTIONPATTERN, filterin)
     if optionsplit:
-        # Split, clean and sort options
+        # If applicable, separate and sort the filter options
         filtertext = removeunnecessarywildcards(optionsplit.group(1))
         optionlist = optionsplit.group(2).lower().replace("_", "-").split(",")
 
@@ -212,11 +219,10 @@ def filtertidy (filterin):
             # Check for the match-case option
             if option == "match-case":
                 case = True
-        # Remove any special options
-        [optionlist.remove(option) for option in removeentries]
         # Sort all options other than domain alphabetically
+        [optionlist.remove(option) for option in removeentries]
         optionlist = sorted(set(optionlist), key=lambda option: option.strip("~"))
-        # If applicable, sort domain restrictions and add the option to the end of the list
+        # If applicable, sort domain restrictions and add them, with the relevant option, to the end of the list of options
         if domainlist:
             optionlist.append("domain={domainlist}".format(domainlist = "|".join(sorted(set(domainlist), key=lambda domain: domain.strip("~")))))
 
@@ -224,19 +230,22 @@ def filtertidy (filterin):
         if not case:
             filtertext = filtertext.lower()
 
-        # Add the options back to the filter and return it
+        # Return the full filter
         return "{filtertext}${options}".format(filtertext = filtertext, options = ",".join(optionlist))
     else:
         # Remove unnecessary asterisks and return the filter
         return removeunnecessarywildcards(filterin.lower())
 
 def elementtidy (domains, selector):
+    """ Sort the domains of element hiding rules, remove unnecessary
+    tags and make the relevant sections of the rule lower case."""
     # Order domain names alphabetically, ignoring exceptions
     if "," in domains:
         domains = ",".join(sorted(set(domains.split(",")), key=lambda domain: domain.strip("~")))
-    # Mark the beginning and end of the selector in an unambiguous manner
+    # Mark the beginning and end of the selector with "@"
     selector = "@{selector}@".format(selector = selector)
     each = re.finditer
+    # Remove unnecessary tags
     for untag in each(REMOVALPATTERN, selector):
         bc = untag.group(2)
         if bc == None:
@@ -244,7 +253,7 @@ def elementtidy (domains, selector):
         untagname = untag.group(4)
         ac = untag.group(5)
         selector = selector.replace("{before}{untag}{after}".format(before = bc, untag = untagname, after = ac), "{before}{after}".format(before = bc, after = ac), 1)
-    # Make the tags lower case wherever possible
+    # Make the remaining tags lower case wherever possible
     for tag in each(SELECTORPATTERN, selector):
         tagname = tag.group(1)
         ac = tag.group(3)
@@ -256,11 +265,11 @@ def elementtidy (domains, selector):
         pseudoclass = pseudo.group(1)
         ac = pseudo.group(3)
         selector = selector.replace("{pclass}{after}".format(pclass = pseudoclass, after = ac), "{pclass}{after}".format(pclass = pseudoclass.lower(), after = ac), 1)
-    # Remove the markers from the beginning and end of the selector, join the rule once more and return it
+    # Remove the markers from the beginning and end of the selector and return the complete rule
     return "{domain}##{selector}".format(domain = domains, selector = selector[1:-1])
 
 def commit (repotype, location, userchanges):
-    # Only continue if changes have been made to the repository
+    """ Commit changes to a repository using the commands provided."""
     difference = subprocess.check_output(repotype[0])
     if not difference:
         print("\nNo changes have been recorded by the repository.")
@@ -273,19 +282,19 @@ def commit (repotype, location, userchanges):
             comment = str(input("Please enter a valid commit comment or quit:\n"))
             if checkcomment(comment, userchanges):
                 break
-    # Allow users to abort
+    # Allow users to abort the commit process
     except (KeyboardInterrupt, SystemExit):
         print("\nCommit aborted.")
         return
 
     print("Comment \"{comment}\" accepted.".format(comment = comment))
     try:
-        # Commit changes
+        # Commit the changes
         command = list(repotype[1])
         command.append(comment)
         subprocess.Popen(command).communicate()
         print("\nConnecting to server. Please enter your password if required.")
-        # Update the server as required by the revision control system
+        # Update the server repository as required by the revision control system
         for command in repotype[2:]:
             subprocess.Popen(command).communicate()
             print("")
@@ -298,18 +307,19 @@ def commit (repotype, location, userchanges):
     print("Completed commit process successfully.")
 
 def isglobalelement (domainlist):
-    # Check whether all domains are negations
+    """ Check whether all domains are negations."""
     for domain in domainlist:
         if domain != "" and not domain[0] == "~":
             return False
     return True
 
 def removeunnecessarywildcards (filtertext):
+    """ Where possible, remove unnecessary wildcards from the beginnings
+    and ends of blocking filters."""
     whitelist = False
     if filtertext[0:1] == "@@":
         whitelist = True
         filtertext = filtertext[2:]
-    # Remove wildcards from the beginning of the filter
     while True:
         if filtertext[0] != "*":
             break
@@ -319,7 +329,6 @@ def removeunnecessarywildcards (filtertext):
                 break
             else:
                 filtertext = proposed
-    # Remove wildcards from the end of the filter
     while True:
         if filtertext[-1] != "*":
             break
@@ -334,6 +343,8 @@ def removeunnecessarywildcards (filtertext):
     return filtertext
 
 def checkcomment(comment, changed):
+    """ Check the commit comment and return True if the comment is
+    acceptable and False if it is not."""
     sections = parts(COMMITPATTERN, comment)
     if sections == None:
         print("The comment \"{comment}\" is not in the recognised format.".format(comment = comment))
@@ -350,17 +361,19 @@ def checkcomment(comment, changed):
             if not validurl(address):
                 print("Unrecognised address \"{address}\".".format(address = address))
             else:
-                # The user has changed the subscription and selected a suitable comment message with a valid address
+                # The user has changed the subscription and has selected a suitable comment message with a valid address
                 return True
     print("")
     return False
 
 def validurl (url):
+    """ Check that an address has a scheme (e.g. http), a domain name
+    (e.g. example.com) and a path (e.g. /), or relates to the internal
+    about system."""
     addresspart = urlparse(url)
-    # Require that an address has a scheme, domain name and path
     if addresspart.scheme and addresspart.netloc and addresspart.path:
         return True
-    elif addresspart.scheme == "about" and addresspart.path:
+    elif addresspart.scheme == "about":
         return True
     else:
         return False
