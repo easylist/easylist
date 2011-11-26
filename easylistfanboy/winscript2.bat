e9ba3e870f0f67930ff95fdd6aeb15fd02e672ee
mp3geek@gmail.com <mp3geek@gmail.com>
2011-11-26 22:56:52 +1300

    move sort script to the top

--- easylistfanboy/winscript2.bat
+++ easylistfanboy/winscript2.bat
@@ -1,32 +1,5 @@
 @echo off
 hg pull -u
-:: Firefox
-perl scripts\addChecksum.pl fanboy-adblocklist-current-expanded.txt
-perl scripts\addChecksum.pl fanboy-adblocklist-stats.txt
-perl scripts\addChecksum.pl fanboy-adblocklist-addon.txt
-perl scripts\addChecksum.pl adblock-gannett.txt
-perl scripts\addChecksum.pl other\chrome-addon.txt
-perl scripts\addChecksum.pl enhancedstats-addon.txt
-perl scripts\addChecksum.pl other\tracking-intl.txt
-:: Firefox Regional lists
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-chn.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-cz.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-esp.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-ita.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-jpn.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-krn.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-rus-v2.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-swe.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-tky.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-vtn.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-ind.txt
-perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-pol.txt
-:: Opera
-perl scripts\addChecksum-opera.pl opera\urlfilter.ini
-:: Internet Explorer
-perl scripts\addChecksum.pl ie\fanboy-adblock-addon.txt
-perl scripts\addChecksum.pl ie\fanboy-tracking-addon.txt
-perl scripts\addChecksum.pl ie\fanboy-russian-addon.txt
 :: pre-sort
 perl docs\sorting\sorter.pl fanboy-adblocklist-current-expanded.txt
 perl docs\sorting\sorter.pl fanboy-adblocklist-stats.txt
@@ -64,6 +37,33 @@ perl docs\sorting\sorter.pl opera\urlfilter-swe.ini
 perl docs\sorting\sorter.pl ie\fanboy-adblock-addon.txt
 perl docs\sorting\sorter.pl ie\fanboy-tracking-addon.txt
 perl docs\sorting\sorter.pl ie\fanboy-russian-addon.txt
+:: Firefox
+perl scripts\addChecksum.pl fanboy-adblocklist-current-expanded.txt
+perl scripts\addChecksum.pl fanboy-adblocklist-stats.txt
+perl scripts\addChecksum.pl fanboy-adblocklist-addon.txt
+perl scripts\addChecksum.pl adblock-gannett.txt
+perl scripts\addChecksum.pl other\chrome-addon.txt
+perl scripts\addChecksum.pl enhancedstats-addon.txt
+perl scripts\addChecksum.pl other\tracking-intl.txt
+:: Firefox Regional lists
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-chn.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-cz.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-esp.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-ita.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-jpn.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-krn.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-rus-v2.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-swe.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-tky.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-vtn.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-ind.txt
+perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-pol.txt
+:: Opera
+perl scripts\addChecksum-opera.pl opera\urlfilter.ini
+:: Internet Explorer
+perl scripts\addChecksum.pl ie\fanboy-adblock-addon.txt
+perl scripts\addChecksum.pl ie\fanboy-tracking-addon.txt
+perl scripts\addChecksum.pl ie\fanboy-russian-addon.txt
 :: Commit
 hg commit -m "%*"
 hg push
